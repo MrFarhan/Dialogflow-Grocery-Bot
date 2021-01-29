925c98da27077415a83ff4b72194e812ca45d1fe
MrFarhan <muhammadfarhan012345@gmail.com>
2021-01-30 03:53:10 +0500

    successfully connected to google sheets

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const { WebhookClient } = require("dialogflow-fulfillment");
 const app = express().use(bodyParser.json());
 
 const { GoogleSpreadsheet } = require("google-spreadsheet");
+const service = require("./service_Account.json");
 
 const doc = new GoogleSpreadsheet(
+    service.api_key
 );
+
+
+(async function () {
+    await doc.useServiceAccountAuth({
+        client_email: service.client_email,
+        private_key: service.private_key,
+    });
+    await doc.loadInfo(); // loads document properties and worksheets
+    console.log(doc.title);
+}());
+
+
+
 
 var port = process.env.PORT || 3000;
 
@@ -18,7 +33,7 @@ app.post("/webhook", (request, response) => {
     const _agent = new WebhookClient({ request, response });
     let queryText = request.body.queryResult.queryText;
 
-    
+
     function Welcome(agent) {
         agent.add(
             "Hi, welcome intent from webhook triggered"
