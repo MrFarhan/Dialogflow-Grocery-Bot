dd7229705da1438e842f1ae0509ca0b0c4a5e3f5
MrFarhan <muhammadfarhan012345@gmail.com>
2021-01-30 02:00:57 +0500

    initial setup updated

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express().use(bodyParser.json());
 const { WebhookClient } = require("dialogflow-fulfillment");
-const serverless = require("serverless-http");
+const app = express().use(bodyParser.json());
+
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
-const creds = require("./client_secret.json");
 const doc = new GoogleSpreadsheet(
 );
+
+var port = process.env.PORT || 3000;
 
 app.get("/", (request, response) => {
     response.send("Hello!");
 });
 
 app.post("/webhook", (request, response) => {
-    const agent = new WebhookClient({ request, response });
+    const _agent = new WebhookClient({ request, response });
     let queryText = request.body.queryResult.queryText;
 
     
     function Welcome(agent) {
-        if (agent.originalRequest.source === "GOOGLE_TELEPHONY") {
-            console.log("Welcome intent for dialogflow telephony is triggered");
-            return agent.add(
-                "Hi, this is a conversational survey. It'll just take a minute. Do you subscribe to any on-demand video streaming services?"
-            );
-        }
         agent.add(
-            "Hi, this is a Conversational Survey. It'll just take a minute"
+            "Hi, welcome intent from webhook triggered"
         );
-        agent.add("Do you subscribe to any on-demand video streaming services?");
     }
 
     let intents = new Map();
     intents.set("Default Welcome Intent", Welcome);
 
-    agent.handleRequest(intents);
+    _agent.handleRequest(intents);
 });
 
-// app.listen(3000, () => {
-//     console.log("server running on port " + 3000);
-// });
-
-module.exports.handler = serverless(app);
+app.listen(port, () => {
+    console.log("server running on port " + port);
+});
